6361daac3ead8792d3e0036f31445729bf6208c0
Tomom7 <omotyasub@icloud.com>
2022-11-11 22:00:27 +0900

    試行錯誤

--- system.py
+++ system.py
@@ -2,22 +2,21 @@
 import io, sys
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
+
 # Pythonでもデータ構造が欲しいため、「dataclasses」を利用してみる
 from dataclasses import dataclass
 
 @dataclass
 class Person:
-    name: str
+    name: str = 'XXX'
 
-def printPerson( Person ):
-    print( 'name:' + (Person.name))
     
 def main():  
     # person_count = int(input("部屋の人数を入力\n"))
-    Person.name = input("名前を入力：\n")
     
-    printPerson( Person )
-
+    person1 = input("名前を入力：\n")
+    dataclass person1 = Person()
+    Person.name = input("名前を入力：\n")
 
 # Pythonとして実行された場合のみ、main関数を利用する
 if __name__ == '__main__':

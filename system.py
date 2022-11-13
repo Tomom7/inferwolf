0ae520097a524547cb16ce98e141a689bfd5cf60
Tomom7 <omotyasub@icloud.com>
2022-11-13 16:13:53 +0900

    微修正

--- system.py
+++ system.py
@@ -1,5 +1,8 @@
 # input文が文字化けしていたため、標準出力と標準エラー出力をutf-8で出力するようにencodingを指定
+import os
+
 import io, sys
+from flask import Flask, flash, redirect, render_template, request, session
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
 
@@ -17,13 +20,12 @@ def printPerson( e ):
 
     
 def main():  
-    input_person = input("部屋の人数を入力\n")
-    person_count = int(input_person)
+    person_count = (int(input("部屋の人数を入力\n")))
     
-    person = list(person_count)
-    for i in range(len(person)):
+    person = list(range(person_count))
+    for i in range(person_count):
         person[i] = Person('(unclear)', '(unclear)', '(unclear)',)
-        
+        3
 
     person[0].name = 'Tom'
     for i in range(len(person)):

d872a56ced7e5313fa7cd40bfa104749cb0a54c8
Tomom7 <omotyasub@icloud.com>
2022-12-06 21:25:16 +0900

    ファイルの分割の叩き上げ策

--- system.py
+++ system.py
@@ -3,6 +3,8 @@ import io, sys
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
 
+import input
+
 # Pythonでもデータ構造が欲しいため、「dataclasses」を利用してみる
 from dataclasses import dataclass
 
@@ -19,6 +21,7 @@ def printPerson( e ):
     print( '名前:' + str(e.name) + ',' + '役職:' + str(e.position) + ',' + '宣言役職:' + str(e.say_position))
 
 
+    
 def main():  
     input_person = input("部屋の人数を入力\n")
     person_count = int(input_person)
@@ -27,7 +30,7 @@ def main():
     for i in person:
         person[i] = Person('(unclear)', '(unclear)', '(unclear)', False, False)
         
-
+    input.main
     person[0].name = 'Tom'
     for i in range(len(person)):
         printPerson( person[i] )
